48c6b9501022d3560b33ebc24288f69027fa85f3
Nixmo4 <109420271+Nixmo4@users.noreply.github.com>
2024-11-20 19:32:27 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,267 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.406130023663726,
+        "pitch": -0.01470383558944377,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6489349830792328,
+          "pitch": 0.1386045630106345,
+          "rotation": 6.283185307179586,
+          "target": "3-dining"
+        },
+        {
+          "yaw": -0.24954807947772473,
+          "pitch": 0.03133237595723415,
+          "rotation": 0.7853981633974483,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": 1.5844852920900276,
+          "pitch": 0.24480682499921969,
+          "rotation": 7.0685834705770345,
+          "target": "2-courtyard"
+        },
+        {
+          "yaw": 0.9435696716649691,
+          "pitch": -0.040102035634905775,
+          "rotation": 0,
+          "target": "4-formal-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6076728800953735,
+        "pitch": -0.07272487907938263,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4657201981960206,
+          "pitch": 0.06209459327830302,
+          "rotation": 1.5707963267948966,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 3.0177626865409675,
+          "pitch": 0.05012463280200308,
+          "rotation": 0,
+          "target": "3-dining"
+        },
+        {
+          "yaw": -2.943609894904906,
+          "pitch": 0.06892037450686495,
+          "rotation": 0,
+          "target": "2-courtyard"
+        },
+        {
+          "yaw": 2.785043496911026,
+          "pitch": 0.022121023090701186,
+          "rotation": 4.71238898038469,
+          "target": "4-formal-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-courtyard",
+      "name": "Courtyard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.22957792468541527,
+        "pitch": 0.30878054737837246,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.500406616994283,
+          "pitch": -0.04024198568388471,
+          "rotation": 1.5707963267948966,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": -2.184594506680485,
+          "pitch": 0.1306370771367611,
+          "rotation": 0,
+          "target": "3-dining"
+        },
+        {
+          "yaw": 3.0281821419594532,
+          "pitch": 0.04286475862356376,
+          "rotation": 4.71238898038469,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dining",
+      "name": "Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9263987981782762,
+        "pitch": 0.44111506768338415,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1731216081913196,
+          "pitch": 0.008919237170836425,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 1.4038634368315446,
+          "pitch": -0.02746105926751774,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": -0.2285725964549794,
+          "pitch": 0.3455598874802739,
+          "rotation": 4.71238898038469,
+          "target": "2-courtyard"
+        },
+        {
+          "yaw": 2.9951543232869264,
+          "pitch": 0.06616138600728405,
+          "rotation": 4.71238898038469,
+          "target": "4-formal-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-formal-living",
+      "name": "Formal Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.02481863901968495,
+        "pitch": 0.03748907056789896,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8731590409831416,
+          "pitch": 0.14358558028908064,
+          "rotation": 0.7853981633974483,
+          "target": "3-dining"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Youtube 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
